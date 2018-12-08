622b29dbaa6bf0f27905dbbb6a58e19374fff35a
Dskfj <646631914@qq.com>
2018-12-08 23:48:21 +0800

    提交的代码信息

---
+++ src/com/software/test/moyu.java
@@ -0,0 +1,37 @@
+package com.software.test;
+
+import com.software.entity.User;
+import com.software.jdbcutil.JDBCUtils;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.junit.jupiter.api.Test;
+
+
+class FirstTest {
+
+	@Test
+	void test() {
+		// fail("Not yet implemented");
+
+		String sql1 = "select * from users where username = ?";
+		
+		String username = "4225111212";
+		
+		QueryRunner queryRunner = new QueryRunner();
+		java.sql.Connection conn = null;
+		
+		try {
+			conn = JDBCUtils.getConnection();
+
+			User user = queryRunner.query(conn, sql1, new BeanHandler<User>(User.class), username);
+			System.out.println(user.toString());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally{
+			JDBCUtils.closeConnection(conn);
+		}
+		
+	}
+
+}
